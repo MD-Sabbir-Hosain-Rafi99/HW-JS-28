4f5aaa1553a9d0c24bb71455273541325da0473d
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-04-10 21:49:04 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/* 1. There are three types of variable in Javascript 
+   var,let,const
+*/
+var numberOne = 4444;
+var numberTwo = 5555;
+let developerName = "Al-Amin Sir";
+let securityName = "MD Sabbir Hosain Rafi";
+const studentID =1
